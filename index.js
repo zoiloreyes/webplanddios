c47ebfca6e300341866e3ac6c88b28b9527a2e5b
Zoilo Reyes Fedora <zoiloismaelreyes1@gmail.com>
2016-12-26 18:22:18 -0400

    Añadido prueba de slideshow

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var slideIndex = 1;
+showSlides(slideIndex);
 function displayMenu(){
 	var x = document.getElementbyId("iglnav");
 	if(x.className === "top-nav"){
@@ -5,4 +7,28 @@ function displayMenu(){
 	} else{
 		x.className = "top-nav";
 	}
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
