3f32721df92d99bfb7845ab06d2060e89a01e4ca
Zoilo Reyes Fedora <zoiloismaelreyes1@gmail.com>
2016-12-26 19:57:36 -0400

    Arreglado problema de despliegue, eliminado el efecto fade del slideshow

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+
+$( document ).ready(function(){
+	showSlides(1);
+});
 var slideIndex = 1;
-showSlides(slideIndex);
+
+
 function displayMenu(){
 	var x = document.getElementbyId("iglnav");
 	if(x.className === "top-nav"){
@@ -8,7 +13,6 @@ function displayMenu(){
 		x.className = "top-nav";
 	}
 }
-
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -18,17 +22,17 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+	  var i;
+	  var slides = document.getElementsByClassName("mySlides");
+	  var dots = document.getElementsByClassName("dot");
+	  if (n > slides.length) {slideIndex = 1}
+	  if (n < 1) {slideIndex = slides.length}
+	  for (i = 0; i < slides.length; i++) {
+	      slides[i].style.display = "none";
+	  }
+	  for (i = 0; i < dots.length; i++) {
+	      dots[i].className = dots[i].className.replace(" active", "");
+	  }
+	  slides[slideIndex-1].style.display = "block";
+	  dots[slideIndex-1].className += " active";
 }
